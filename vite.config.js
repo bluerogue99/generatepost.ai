7f3a86f6ffc315572d4b026e88aecae952622543
Blue Rogue <info@jozsefsimco.com>
2024-11-11 14:39:02 +0100

    Vite build settings

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,36 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-    server: {
-        port: 3000
-      }
+export default defineConfig(({ command }) => {
+    if (command === 'build') {
+        return {
+            plugins: [
+                laravel({
+                    input: 'resources/js/app.jsx',
+                    refresh: true,
+                }),
+                react(),
+            ],
+            resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'], 
+            },
+            build: {
+                outDir: 'public_html/build', 
+                assetsDir: 'assets',
+            },
+        };
+    } else {
+        return {
+            plugins: [
+                laravel({
+                    input: 'resources/js/app.jsx',
+                    refresh: true,
+                }),
+                react(),
+            ],
+            server: {
+                port: 3000,
+            },
+        };
+    }
 });
